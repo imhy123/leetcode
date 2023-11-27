13ff1200f3fae306846216dcdeade4ea33a20f84
Spirithy <spirithy@gmail.com>
2023-11-27 19:27:45 +0800

    first commit; 0003 resolved;

---
+++ lc0003.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+	var length = len(s)
+
+	if length <= 0 {
+		return 0
+	}
+
+	var i, j, ret int = 0, 0, 0
+	var bitmap [128]int
+
+	bitmap[s[j]] = 1
+
+	for j < length {
+		j++
+		if j == length {
+			break
+		}
+
+		if bitmap[s[j]] == 1 {
+			if j-i > ret {
+				ret = j - i
+			}
+
+			var matched = false
+			for i < j {
+				if matched {
+					break
+				}
+
+				matched = s[i] == s[j]
+
+				bitmap[s[i]] = 0
+				i++
+			}
+		}
+
+		bitmap[s[j]] = 1
+	}
+
+	if j-i > ret {
+		ret = j - i
+	}
+
+	return ret
+}
+
+func main() {
+	var s string
+
+	fmt.Scanln(&s)
+
+	fmt.Println(lengthOfLongestSubstring(s))
+}
