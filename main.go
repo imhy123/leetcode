c91dbb8e4335a96e22d3c05d7bdc60f9b230bd17
Spirithy <spirithy@gmail.com>
2023-11-28 14:27:41 +0800

    feat: 0215堆排序resolved;

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "spirithy.com/leetcode/lc0206"
+import (
+	"fmt"
+
+	"spirithy.com/leetcode/lc0215"
+)
 
 func main() {
-	lc0206.Target()
+	//lc0206.Target()
+
+	ret := lc0215.Target([]int{-1, 2, 0}, 2)
+	fmt.Println(ret)
 }
