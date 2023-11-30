5625c51518619359cd4080e00c08728997dde53f
heyong.hy <heyong.hy@alibaba-inc.com>
2023-11-30 13:38:17 +0800

    feat: 215使用快速选择；

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
-	"spirithy.com/leetcode/lc0215"
+	"spirithy.com/leetcode/lc0215a"
 )
 
 func main() {
 	//lc0206.Target()
 
-	ret := lc0215.Target([]int{-1, 2, 0}, 2)
+	//ret := lc0215.Target([]int{-1, 2, 0}, 2)
+	//fmt.Println(ret)
+
+	ret := lc0215a.Target([]int{-1, 2, 0}, 3)
 	fmt.Println(ret)
 }
