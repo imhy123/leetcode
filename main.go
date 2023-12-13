9c41990b8af0e5150b6a52dc14a3234365a34802
heyong.hy <heyong.hy@alibaba-inc.com>
2023-12-14 01:54:51 +0800

    feat: 146 resolved;

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"spirithy.com/leetcode/lc0215b"
+	"spirithy.com/leetcode/lc0146"
 )
 
 func main() {
@@ -12,6 +10,8 @@ func main() {
 	//ret := lc0215.Target([]int{-1, 2, 0}, 2)
 	//fmt.Println(ret)
 
-	ret := lc0215b.Target([]int{7, 6, 5, 4, 3, 2, 1}, 5)
-	fmt.Println(ret)
+	//ret := lc0215b.Target([]int{7, 6, 5, 4, 3, 2, 1}, 5)
+	//fmt.Println(ret)
+
+	lc0146.TestCase()
 }
